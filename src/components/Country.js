fd7f8cf7acad18240e925682c6db88a1e73047e2
Masoud <mas.sam1368@gmail.com>
2022-09-09 16:54:59 +0430

    add image lazy load

--- src/components/Country.js
+++ src/components/Country.js
@@ -1,11 +1,12 @@
 import { Link } from "react-router-dom";
+import { LazyLoadImage } from 'react-lazy-load-image-component';
 
 const Country = ({ country, countries }) => {
 
   return (
     <Link className="country" to={`/${country.alpha3Code}`} state={{  country, countries  }}>
       <li>
-        <img src={country.flags.svg} alt={`${country.name} flag`}/>
+        <LazyLoadImage effect="blur" src={country.flags.svg} alt={`${country.name} flag`}/>
         <h2> {country.name}</h2>
         <h3><span className='boldHeading'>Population: </span> {parseInt(country.population).toLocaleString()}</h3>
         <h3><span className='boldHeading'>Region: </span> {country.region}</h3>
